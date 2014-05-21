7b14340124d78a189c05a62cc7700075c62b71a0
benmarx95 <benmarx95@gmail.com>
2014-05-20 17:00:24 -0700

    Initial Commit

---
+++ AdventureGame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AdventureGame
+//
+//  Created by Ben Marx on 5/20/14.
+//  Copyright (c) 2014 Ben Marx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
